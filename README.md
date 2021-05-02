1d8af94ce3b12eb17aa72004c75758fcf1f14278
Serge Fonville <serge.fonville@gmail.com>
2021-05-02 04:31:04 +0200

    Started structure update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Gate IO API implementation in Rust
 
-The API implementation is based on gateapi-python
+The API implementation is initially based on gateapi-python, complemented with the structure of gateapi-csharp.
---
+++ src/gate_api/model/candlestick.rs
@@ -0,0 +1,19 @@
+pub struct Candlestick {
+    timestamp: u32,
+    volume: f32,
+    close: f32,
+    highest: f32,
+    lowest: f32,
+    open: f32,
+}
+
+impl Candlestick {
+    pub new(timestamp: u32,
+        volume: f32,
+        close: f32,
+        highest: f32,
+        lowest: f32,
+        open: f32) -> Candlestick {
+            Candlestick {timestamp, volume, close, highest, lowest, open}
+        }
+}
